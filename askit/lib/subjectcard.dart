d720d469374cd31e216dfd4c14dd6bb1eb49da68
Umairus <mumair.bscs19seecs@seecs.edu.pk>
2022-08-07 16:56:35 +0500

    Restyled app and added dummy data

--- askit/lib/subjectcard.dart
+++ askit/lib/subjectcard.dart
@@ -1,41 +1,37 @@
 import 'package:flutter/material.dart';
-import 'colorpallete.dart';
-import "dart:math";
 
 class SubjectCard extends StatelessWidget {
   // const SubjectCard({Key? key}) : super(key: key);
   String subject;
-  static int colorIndex = 0;
-  SubjectCard(this.subject);
+  Color color;
+
+  SubjectCard(this.subject, this.color);
 
   void selectSubject(BuildContext ctx) {
     print(subject + " Tapped");
     print(ctx.size);
   }
 
-  void deleteSubject(BuildContext ctx) {}
+// onTap: () => selectSubject(context),
 
   @override
   Widget build(BuildContext context) {
-    colorIndex = colorIndex + 1;
-    if (colorIndex > 4) {
-      colorIndex = 1;
-    }
-    print(colorIndex);
-    return AspectRatio(
-      aspectRatio: 1,
+    return Container(
       child: InkWell(
         onTap: () => selectSubject(context),
-        splashColor: Colors.cyan,
-        child: Container(
+        splashColor: Colors.greenAccent,
+        child: Expanded(
           child: Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-            color: colors[colorIndex],
-            child: Center(
-              child: Text(subject,
-                  textScaleFactor: 1.5,
-                  style: TextStyle(backgroundColor: Colors.white10)),
+            child: Container(
+              child: Row(
+                children: [
+                  Text(subject),
+                  Text(
+                    "Tap to explore",
+                    textScaleFactor: 0.5,
+                  )
+                ],
+              ),
             ),
           ),
         ),
